2520b40dcc5fcd9666c59fbbf5a89fb6fe191b71
sruthi <sruthikallingal@gmail.com>
2023-03-10 16:14:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {Firebase} from './FIREBASE/config'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>{
+        Firebase.firestore().collection('products').get().then(snapshot=>{
+          snapshot.forEach(obj => {
+            console.log(obj.data())
+          });
+        })
+      }}>Click me</button>
     </div>
   );
 }
---
+++ src/FIREBASE/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"; 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAADOkYEU6HmRZxynaBrcpnjg0julkqHjY",
+    authDomain: "fir-34af4.firebaseapp.com",
+    projectId: "fir-34af4",
+    storageBucket: "fir-34af4.appspot.com",
+    messagingSenderId: "282329373014",
+    appId: "1:282329373014:web:a014cd2173c3d711a7def7",
+    measurementId: "G-Q1EZ1J8ZMM"
+  };
+
+  export const Firebase = initializeApp(firebaseConfig); 
